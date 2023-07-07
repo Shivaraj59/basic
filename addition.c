d672df27e273f818fd9d4d4dab19d82a983fd2f2
shivaraj59 <mshivashree@gmail.com>
2023-07-07 11:35:43 +0530

    Addition

---
+++ addition.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+int main()
+{
+	int a,b;
+	printf("Enter the first number :");
+	scanf("%d",&a);
+        printf("Enter the second number :");
+        scanf("%d",&b);
+	printf("sum of two numbers :%d\n",a+b);
+
+	return 0;
+}
